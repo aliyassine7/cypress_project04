e35ae923f81c4e647a417379182cafb07b0c6317
aliyassine7 <aliyassine43@gmail.com>
2024-06-03 22:52:31 -0500

    added test 1

--- cypress/e2e/projects/cypress_project04.cy.js
+++ cypress/e2e/projects/cypress_project04.cy.js
@@ -4,29 +4,29 @@ import ToDoPage from "../../pages/ToDoPage";
 
 const toDoPage = new ToDoPage();
 
-describe('ToDo List Form Tests', () => {
+describe("ToDo List Form Tests", () => {
   beforeEach(() => {
-    cy.clickCard('Project - Todo List')
-  })
-
-  it.only('Test Case 01 - Todo-App Modal Verification', () => {
-    toDoPage.getToDoAppModalTitle().should('have.text', 'My Tasks')
+    cy.clickCard("Project - Todo List");
   });
 
-  it('Test Case 02 - Single Task Addition and Removal', () => {
-
+  it("Test Case 01 - Todo-App Modal Verification", () => {
+    toDoPage.getToDoAppModalTitle().should("have.text", "My Tasks");
+    toDoPage.getNewToDoInputField().should("be.enabled");
+    toDoPage.getAddButton().should("be.enabled");
+    toDoPage.getSearchField().should("be.enabled");
+    toDoPage
+      .getTaskList()
+      .should("have.text", "No tasks found!")
+      .and("have.length", "1");
   });
 
-  it('Test Case 03 - Multiple Task Operations', () => {
-
+  it.only("Test Case 02 - Single Task Addition and Removal", () => {
+    
   });
 
-  it('Test Case 04 - Search and Filter Functionality in todo App', () => {
+  it("Test Case 03 - Multiple Task Operations", () => {});
 
-  });
+  it("Test Case 04 - Search and Filter Functionality in todo App", () => {});
 
-  it('Test Case 05 - Task Validation and Error Handling', () => {
-
-  });
-
+  it("Test Case 05 - Task Validation and Error Handling", () => {});
 });
--- cypress/pages/ToDoPage.js
+++ cypress/pages/ToDoPage.js
@@ -8,19 +8,27 @@ class ToDoPage {
   }
 
   getNewToDoInputField() {
-    return
+    return cy.get('#input-add')
+  }
+
+  getAddButton() {
+    return cy.get('#add-btn')
+  }
+
+  getSearchField() {
+    return cy.get('#search')
+  }
+
+  getTaskList() {
+    return cy.get('.todo-item > p')
   }
 
 
 
-
-
-
-
-
-
-
   // Methods
+  typeAndEnterNewTask() {
+    this.getNewToDoInputField()
+  }
 }
 
 export default ToDoPage;
