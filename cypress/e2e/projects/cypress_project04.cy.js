e97fa7c5592126d6bae8066b7a001519d412c7d3
aliyassine7 <aliyassine43@gmail.com>
2024-06-04 00:45:46 -0500

    completed all tests

--- cypress/e2e/projects/cypress_project04.cy.js
+++ cypress/e2e/projects/cypress_project04.cy.js
@@ -4,6 +4,13 @@ import ToDoPage from "../../pages/ToDoPage";
 
 const toDoPage = new ToDoPage();
 
+const validateListIsEmpty = () => {
+  toDoPage
+    .getEmptyTaskList()
+    .should("have.text", "No tasks found!")
+    .and("have.length", 1);
+};
+
 describe("ToDo List Form Tests", () => {
   beforeEach(() => {
     cy.clickCard("Project - Todo List");
@@ -14,19 +21,60 @@ describe("ToDo List Form Tests", () => {
     toDoPage.getNewToDoInputField().should("be.enabled");
     toDoPage.getAddButton().should("be.enabled");
     toDoPage.getSearchField().should("be.enabled");
+    validateListIsEmpty();
+  });
+
+  it("Test Case 02 - Single Task Addition and Removal", () => {
+    toDoPage.typeAndEnterNewTask("task 1");
+    toDoPage.getListItems().should("have.text", "task 1");
+    toDoPage.getListItems().should("have.length", 1);
+    toDoPage.getListButtons().click();
     toDoPage
-      .getTaskList()
-      .should("have.text", "No tasks found!")
-      .and("have.length", "1");
+      .getSpecificListIcon(0)
+      .should("have.attr", "class", "panel-icon has-text-success");
+    toDoPage.clickRemoveCompletedTasksButton();
+    validateListIsEmpty();
   });
 
-  it.only("Test Case 02 - Single Task Addition and Removal", () => {
-    toDoPage.typeAndEnterNewTask('this is my new task')
+  const list = ["task 1", "task 2", "task 3", "task 4", "task 5"];
+
+  it("Test Case 03 - Multiple Task Operations", () => {
+    list.forEach((elem, index) => {
+      toDoPage.typeAndEnterNewTask(elem);
+      toDoPage.getSpecificListItems(index).should("have.text", elem);
+      toDoPage.clickListButtons(index);
+    });
+    toDoPage.clickRemoveCompletedTasksButton();
+    validateListIsEmpty();
   });
 
-  it("Test Case 03 - Multiple Task Operations", () => {});
+  it("Test Case 04 - Search and Filter Functionality in todo App", () => {
+    list.forEach((elem, index) => {
+      toDoPage.typeAndEnterNewTask(elem);
+      toDoPage.getSpecificListItems(index).should("have.text", elem);
+    });
+    toDoPage.typeInSearchBar(list.at(-1));
+    toDoPage
+      .getListItems()
+      .should("have.length", 1)
+      .and("have.text", list.at(-1));
+  });
 
-  it("Test Case 04 - Search and Filter Functionality in todo App", () => {});
-
-  it("Test Case 05 - Task Validation and Error Handling", () => {});
+  it.only("Test Case 05 - Task Validation and Error Handling", () => {
+    toDoPage.typeAndEnterNewTask("");
+    validateListIsEmpty();
+    toDoPage.typeAndEnterNewTask("1234567891011121314151617181920");
+    toDoPage
+      .getErrorMessage()
+      .should("have.text", "Error: Todo cannot be more than 30 characters!");
+    toDoPage.typeAndEnterNewTask(list[0]);
+    toDoPage.getListItems().should("have.length", 1);
+    toDoPage.typeAndEnterNewTask(list[0]);
+    toDoPage
+      .getErrorMessage()
+      .should(
+        "have.text",
+        `Error: You already have ${list[0]} in your todo list.`
+      );
+  });
 });
--- cypress/pages/ToDoPage.js
+++ cypress/pages/ToDoPage.js
@@ -1,33 +1,66 @@
 /// <reference types="cypress" />
 
 class ToDoPage {
-
   // Locators
   getToDoAppModalTitle() {
-    return cy.get('.panel-heading')
+    return cy.get(".panel-heading");
   }
 
   getNewToDoInputField() {
-    return cy.get('#input-add')
+    return cy.get("#input-add");
   }
 
   getAddButton() {
-    return cy.get('#add-btn')
+    return cy.get("#add-btn");
   }
 
   getSearchField() {
-    return cy.get('#search')
+    return cy.get("#search");
   }
 
-  getTaskList() {
-    return cy.get('.todo-item > p')
+  getEmptyTaskList() {
+    return cy.get(".todo-item > p");
   }
 
+  getListItems() {
+    return cy.get("#panel");
+  }
 
+  getSpecificListItems(index) {
+    return this.getListItems().children().eq(index);
+  }
+
+  getListButtons() {
+    return cy.get(".mr-auto .panel-icon");
+  }
+
+  getSpecificListIcon(index) {
+    return this.getListButtons().eq(index);
+  }
+
+  getRemoveCompletedTasksButton() {
+    return cy.get("#clear");
+  }
+
+  getErrorMessage() {
+    return cy.get(".notification");
+  }
 
   // Methods
   typeAndEnterNewTask(message) {
-    this.getNewToDoInputField().type(`${message}{enter}`)
+    this.getNewToDoInputField().clear().type(`${message}{enter}`);
+  }
+
+  clickRemoveCompletedTasksButton() {
+    this.getRemoveCompletedTasksButton().click();
+  }
+
+  clickListButtons(index) {
+    this.getSpecificListIcon(index).click();
+  }
+
+  typeInSearchBar(input) {
+    this.getSearchField().clear().type(input);
   }
 }
 
